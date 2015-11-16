f7ccb30fa3111978ff84e9462eb9e4e749085278
Li Linhao <lhli@thoughtworks.com>
2015-11-16 15:23:20 +0800

    Bind functional button methods.

--- app/src/main/java/com/tw/lhli/calculator/MainActivity.java
+++ app/src/main/java/com/tw/lhli/calculator/MainActivity.java
@@ -2,18 +2,114 @@ package com.tw.lhli.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText resultText;
 
+    Button buttonPlus;
+    Button buttonMinus;
+    Button buttonMultiply;
+    Button buttonDivide;
+    Button buttonDot;
+    Button buttonEqual;
+    Button buttonDelete;
+
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultText = (EditText) findViewById(R.id.result_text);
 
+        buttonPlus = (Button) findViewById(R.id.button_plus);
+        buttonMinus = (Button) findViewById(R.id.button_minus);
+        buttonMultiply = (Button) findViewById(R.id.button_multiply);
+        buttonDivide = (Button) findViewById(R.id.button_divide);
+        buttonDot = (Button) findViewById(R.id.button_dot);
+        buttonEqual = (Button) findViewById(R.id.button_equal);
+        buttonDelete = (Button) findViewById(R.id.button_delete);
+
+        button0 = (Button) findViewById(R.id.button_0);
+        button1 = (Button) findViewById(R.id.button_1);
+        button2 = (Button) findViewById(R.id.button_2);
+        button3 = (Button) findViewById(R.id.button_3);
+        button4 = (Button) findViewById(R.id.button_4);
+        button5 = (Button) findViewById(R.id.button_5);
+        button6 = (Button) findViewById(R.id.button_6);
+        button7 = (Button) findViewById(R.id.button_7);
+        button8 = (Button) findViewById(R.id.button_8);
+        button9 = (Button) findViewById(R.id.button_9);
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText(resultText.getText() + "+");
+            }
+        });
+
+        buttonMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText(resultText.getText() + "-");
+            }
+        });
+
+        buttonMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText(resultText.getText() + "×");
+            }
+        });
+
+        buttonDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText(resultText.getText() + "÷");
+            }
+        });
+
+        buttonDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText(resultText.getText() + ".");
+            }
+        });
+
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultText.setText(deleteLastChar(resultText.getText().toString()));
+            }
+        });
     }
 
-
+    private String deleteLastChar(String resultText) {
+        if (resultText.length() != 0) {
+            return resultText.substring(0, resultText.length()-1);
+        } else {
+            return resultText;
+        }
+    }
 }
