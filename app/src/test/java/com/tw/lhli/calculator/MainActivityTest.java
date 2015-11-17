3eb2767ed982fb8dbc4da70aeb036d0a70fbb202
Li Linhao <lhli@thoughtworks.com>
2015-11-17 16:49:06 +0800

    Added tests to check number buttons clicking events.

--- app/src/test/java/com/tw/lhli/calculator/MainActivityTest.java
+++ app/src/test/java/com/tw/lhli/calculator/MainActivityTest.java
@@ -29,7 +29,7 @@ public class MainActivityTest {
         mainActivity.resultText.setText("This is my first android app.");
         mainActivity.buttonClear.performClick();
 
-        assertEquals(mainActivity.resultText.getText().toString(), "");
+        assertEquals("", mainActivity.resultText.getText().toString());
     }
 
     @Test
@@ -37,25 +37,61 @@ public class MainActivityTest {
         mainActivity.resultText.setText("Hey, Hey, Hey");
         mainActivity.buttonDelete.performClick();
 
-        assertEquals(mainActivity.resultText.getText().toString(), "Hey, Hey, He");
+        assertEquals("Hey, Hey, He", mainActivity.resultText.getText().toString());
+    }
+
+    @Test
+    public void clickingDeleteButton_shouldNotDeleteAnythingWhenResultTextIsEmpty() {
+        mainActivity.resultText.setText("");
+        mainActivity.buttonDelete.performClick();
+
+        assertEquals("", mainActivity.resultText.getText().toString());
     }
 
     @Test
     public void clickingOperatorButton_shouldShowInResultText() throws Exception {
         mainActivity.buttonClear.performClick();
         mainActivity.buttonPlus.performClick();
-        assertEquals(mainActivity.resultText.getText().toString(), "+");
+        assertEquals("+", mainActivity.resultText.getText().toString());
 
         mainActivity.buttonClear.performClick();
         mainActivity.buttonMinus.performClick();
-        assertEquals(mainActivity.resultText.getText().toString(), "-");
+        assertEquals("-", mainActivity.resultText.getText().toString());
 
         mainActivity.buttonClear.performClick();
         mainActivity.buttonMultiply.performClick();
-        assertEquals(mainActivity.resultText.getText().toString(), "×");
+        assertEquals("×", mainActivity.resultText.getText().toString());
 
         mainActivity.buttonClear.performClick();
         mainActivity.buttonDivide.performClick();
-        assertEquals(mainActivity.resultText.getText().toString(), "÷");
+        assertEquals("÷", mainActivity.resultText.getText().toString());
+
+        mainActivity.buttonClear.performClick();
+        mainActivity.buttonLeftBracket.performClick();
+        assertEquals("(", mainActivity.resultText.getText().toString());
+
+        mainActivity.buttonClear.performClick();
+        mainActivity.buttonRightBracket.performClick();
+        assertEquals(")", mainActivity.resultText.getText().toString());
+
+        mainActivity.buttonClear.performClick();
+        mainActivity.buttonDot.performClick();
+        assertEquals(".", mainActivity.resultText.getText().toString());
+    }
+
+    @Test
+    public void clickingNumberButton_shouldShowInResultText() {
+        mainActivity.button0.performClick();
+        mainActivity.button1.performClick();
+        mainActivity.button2.performClick();
+        mainActivity.button3.performClick();
+        mainActivity.button4.performClick();
+        mainActivity.button5.performClick();
+        mainActivity.button6.performClick();
+        mainActivity.button7.performClick();
+        mainActivity.button8.performClick();
+        mainActivity.button9.performClick();
+
+        assertEquals( "0123456789", mainActivity.resultText.getText().toString());
     }
 }
