c0d56587ed4b91cb4cc87809fc226633ab64c1bd
Li Linhao <lhli@thoughtworks.com>
2015-11-20 15:11:24 +0800

    Used retrofit to deal with requests.

--- app/src/main/java/com/tw/lhli/calculator/HistoryActivity.java
+++ app/src/main/java/com/tw/lhli/calculator/HistoryActivity.java
@@ -1,17 +1,21 @@
 package com.tw.lhli.calculator;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -21,12 +25,20 @@ public class HistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_history);
-
         ButterKnife.bind(this);
-
         listView.setAdapter(new CalculatorListAdapter(getData()));
+//        listView.setClickable(true);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String itemText = listView.getItemAtPosition(position).toString();
+//                AlertDialog.Builder alert = new AlertDialog.Builder(HistoryActivity.this);
+//                alert.setTitle(itemText);
+//                AlertDialog alertDialog = alert.create();
+//                alertDialog.show();
+//            }
+//        });
     }
 
     @Override
@@ -38,7 +50,7 @@ public class HistoryActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private List<String> getData(){
+    private List<String> getData() {
         Database database = new Database(this);
 
         return database.selectAllFromDb();
